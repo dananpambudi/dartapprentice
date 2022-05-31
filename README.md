563e6f6103f3ffafdda1036f35389d9fddedc644
dananpambudi <danan.pambudi@tokopedia.com>
2022-05-31 18:36:47 +0700

    initalization & challenge 1

---
+++ README.md
@@ -0,0 +1,3 @@
+solve some exercise from :
+
+https://hackmd.io/@kuzmapetrovich/S1x90jWGP#Exercise-1
---
+++ bin/chapter1/exercise1.dart
@@ -0,0 +1,11 @@
+// “Declare a constant int called myAge and set it equal to your age.
+// Also declare an int variable called dogs and set that equal to the number of dogs you own.
+// Then imagine you bought a new puppy and increment the dogs variable by one.”
+
+void main(List<String> args) {
+  const int myAge = 28;
+  int dogs = 0;
+  dogs++;
+  print(myAge);
+  print(dogs);
+}
